e4b7fa2948ec50ccc1dfb70022f8da00eebca5bb
Caleb Jones <self@calebjones.net>
2016-02-03 00:22:14 -0500

    Add tests and coverage

--- pythonworkflow/__init__.py
+++ pythonworkflow/__init__.py
@@ -1,1 +1,11 @@
 
+def tested_function(a, b):
+    if a == b:
+        return 12
+    else:
+        return 42
+
+
+def untested_function():
+    a = 1 + 2
+    return a
--- tests/test_trivial.py
+++ tests/test_trivial.py
@@ -1,3 +1,9 @@
+import pythonworkflow
 
-def test_trivial():
-    assert True
+
+def test_a():
+    assert pythonworkflow.tested_function(1, 1) == 12
+
+
+def test_b():
+    assert pythonworkflow.tested_function(1, 2) == 42
